58fa364f6a110e8f33cc122f0e6fa6cbf60ff6fd
jl2154 <61812423+jl2154@users.noreply.github.com>
2020-08-11 17:42:58 -0400

    Update PropertyReader.java
    Property Reder

--- src/edu/upenn/cit594/datamanagement/PropertyReader.java
+++ src/edu/upenn/cit594/datamanagement/PropertyReader.java
@@ -1,11 +1,71 @@
 package edu.upenn.cit594.datamanagement;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.upenn.cit594.data.Property;
+import edu.upenn.cit594.data.Ticket;
 
 public class PropertyReader {
-	public List<Property> getAllProperties(){
-		return null;
-	}
+    protected String filename;
+
+    public PropertyReader(String fileName) {
+        this.filename = fileName;
+    }
+
+    public List<Property> getAllProperties() {
+        List<Property> propertyData = new ArrayList<Property>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line = br.readLine();
+            String[] firstRow = line.split(",");
+            Integer zip_codeLocation = 999;
+            Integer total_livable_areaLocation = 999;
+            Integer market_valueLocation = 999;
+            
+            //Finding the location for market_value, total_livable_area, and zip_code fields.
+            for (int i = 0; i < firstRow.length; i++) {
+
+                if (firstRow[i].equalsIgnoreCase("zip_code")) {
+                    zip_codeLocation = i;
+                    break;
+                }
+                if (firstRow[i].equalsIgnoreCase("total_livable_area")) {
+                    total_livable_areaLocation = i;
+                    break;
+
+                }
+                if (firstRow[i].equalsIgnoreCase("market_value")) {
+                    market_valueLocation = i;
+                    break;
+
+                }
+            }
+
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                String[] ticketIdentity = line.split(",");
+
+                Double total_livable_area = Double.parseDouble(ticketIdentity[total_livable_areaLocation].trim());
+                Double market_value = Double.parseDouble(ticketIdentity[market_valueLocation]);
+
+                String zip_code = ticketIdentity[zip_codeLocation].trim().substring(0, 5);// take only first five digit
+                                                                                          // as zipcode
+                propertyData.add(new Property(zip_code, market_value, total_livable_area));
+
+            }
+            return propertyData;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return propertyData;
+
+    }
 }
